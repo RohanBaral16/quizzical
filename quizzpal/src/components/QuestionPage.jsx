30ca7175747f4590d6adc248539923e9b5f6e7e2
rohanbaral16 <rhnbrl16@gmail.com>
2025-09-19 17:48:36 +0545

    app finalized

--- quizzpal/src/components/QuestionPage.jsx
+++ quizzpal/src/components/QuestionPage.jsx
@@ -5,42 +5,82 @@ import {clsx} from'clsx'
 
 export default function QuestionPage(){
     //states
+    const [renderToggle, setRenderToggle] = useState(true)
+
     const [questions, setQuestions] = useState({
-        ...questionsDefault,
     })
 
     const [selectedAnswers, setSelectedAnswer] = useState([])
 
     const [randomizedAnswerArray, setRandomizedAnswerArray] = useState(()=>{
-        const temp = questions.results.map((questionItem)=>{
+        return createRandomAnswerArray()
+    })
+
+    function createRandomAnswerArray(){
+        const temp = questions?.results?.map((questionItem)=>{
             const allAnswers = [...questionItem.incorrect_answers, questionItem.correct_answer]
             const randomizedAnswersResult = randomizeAnswers(allAnswers, questionItem.type)
             return randomizedAnswersResult
-        })
+        }) || []
         return temp
-    })
+    }
+
+    const [gameState, setGameState] = useState(['playing'])
+    // game states are, incomplete, over, and playing
     
     //derived values
+    const correctAnswers = Object.entries(selectedAnswers).filter(([index, answer]) => {
+        return questions?.results[index]?.correct_answer === answer;
+    }).map((data)=>data[1])
 
-    // useEffect(()=>{
-    //     fetch('https://opentdb.com/api.php?amount=5')
-    //         .then(res => res.json())
-    //         .then(data => {
-    //             const decodedData = {
-    //             ...data,
-    //             results: data.results.map(q => ({
-    //                 ...q,
-    //                 question: decodeHtml(q.question),
-    //                 correct_answer: decodeHtml(q.correct_answer),
-    //                 incorrect_answers: q.incorrect_answers.map(a => decodeHtml(a))
-    //             }))
-    //             };
-    //             setQuestions(decodedData);
-    //         })
-    //         .catch(error=>{
-    //             console.error('Fetch error', error)
-    //         })
-    // }, [renderCount])
+    const allCorrectAnswers = questions?.results?.map(questionItem=>{
+        return questionItem.correct_answer
+    }) || []
+
+    let statusText = ''
+    if (gameState==='playing'){
+        statusText = ''
+    }else if(gameState === 'incomplete'){
+        statusText = `Please answer all questions`
+    }else if(gameState === 'over'){
+        statusText =  `You scored ${correctAnswers.length} / ${questions.results.length}`
+    }
+
+    useEffect(() => {
+        if (!questions) return;
+
+        const randomized = questions?.results?.map(q => {
+            const allAnswers = [...q.incorrect_answers, q.correct_answer];
+            return randomizeAnswers(allAnswers, q.type);
+        }) || [];
+
+        setRandomizedAnswerArray(randomized);
+    }, [questions]);
+
+    useEffect(()=>{
+        fetch('https://opentdb.com/api.php?amount=5')
+            .then(res => res.json())
+            .then(data => {
+                const decodedData = {
+                ...data,
+                results: data.results.map(q => ({
+                    ...q,
+                    question: decodeHtml(q.question),
+                    correct_answer: decodeHtml(q.correct_answer),
+                    incorrect_answers: q.incorrect_answers.map(a => decodeHtml(a))
+                }))
+                };
+                setQuestions(decodedData);
+            })
+            .then(()=>{
+                setRandomizedAnswerArray(()=>{
+                    return createRandomAnswerArray()
+                })
+            })
+            .catch(error=>{
+                console.error('Fetch error', error)
+            })
+    }, [renderToggle])
 
     function decodeHtml(html) {
         const txt = document.createElement("textarea");
@@ -50,10 +90,18 @@ export default function QuestionPage(){
     const questionElements = questions?.results?.map((questionItem, index)=>{
         // copy all the answers in an array
 
-        const buttonElements = randomizedAnswerArray[index].map((answer)=>{
+        const buttonElements = randomizedAnswerArray[index]?.map((answer)=>{
+            
+            const isWrong = !correctAnswers.includes(answer) && selectedAnswers[index]===answer && gameState==='over'
+            const isRight =  correctAnswers.includes(answer) && gameState==='over' && selectedAnswers[index]===answer
+            const isNotSelected = selectedAnswers[index]!= answer && gameState === 'over'
+            const isNotSelectedButTrue = isNotSelected && allCorrectAnswers.includes(answer)
             const className=clsx({
                 'each-answer':true,
-                selected: selectedAnswers[index]===answer
+                selected: selectedAnswers[index]===answer,
+                wrong: isWrong,
+                right: isRight || isNotSelectedButTrue,
+                'not-selected': isNotSelected && !isNotSelectedButTrue,
             })
             return(
                 <button 
@@ -65,7 +113,7 @@ export default function QuestionPage(){
                     {answer}
                 </button>
             )
-        })
+        }) || []
         return(
             <section className="each-question-entity" key={index}>
                 <p>{questionItem.question}</p>
@@ -75,7 +123,6 @@ export default function QuestionPage(){
     }) || []
 
     function answerSelected(questionIndex, answer){
-        console.log(questionIndex, answer)
         setSelectedAnswer(prev=>{
             return {
                 ...prev, 
@@ -84,7 +131,22 @@ export default function QuestionPage(){
         })
     }
     function checkAnswer(){
-
+        console.log('selected answers', selectedAnswers)
+        console.log('correct selected answers', correctAnswers)
+        console.log('correct answers', questions.results.map(q=>q.correct_answer))
+        const selectedCount = Object.keys(selectedAnswers).length
+        const correctCount = correctAnswers.length
+        const questionCount = questions.results.length
+        if(selectedCount === questionCount){
+            setGameState('over')
+        }else if(selectedCount < questionCount){
+            setGameState('incomplete')
+        }
+    }
+    function resetGame(){
+        setGameState('playing')  
+        setSelectedAnswer([])     
+        setRenderToggle(prev=>!prev)
     }
 
     return(
@@ -93,8 +155,28 @@ export default function QuestionPage(){
                 <div>{questions? 
                 questionElements
                 : '<p>Loading questions...'}</div>
-                <button className="question-action-btn">Check answers</button>
+                <section className="bottom-section">
+                    <p>{statusText}</p>
+                    {gameState != 'over' && questions.results != null &&
+                    <button 
+                    onClick={checkAnswer}
+                    className="question-action-btn"
+                    >
+                        Check answers
+                    </button>}
 
+                    {gameState === 'over' &&
+                        <button 
+                    onClick={resetGame}
+                    className="question-action-btn"
+                    >
+                        Play again
+                    </button>}
+                    {
+                        !questions.results &&
+                        <h1>loading....</h1>
+                    }
+                </section>
             </section>
         </>
     )
